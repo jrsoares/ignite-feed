13d559c99ecfdc6f1a1466216f1a5a0e96dd60ae
Jr.Soares <juniorsoaresti@gmail.com>
2022-09-23 13:08:05 -0400

    initial structure - global style

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Post } from './Post';
+import './global.css';
+
+function App() {
+  return (
+    <div>
+      <Post
+        author="Jr.Soares"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ratione repudiandae omnis consequatur ipsa dolorum consectetur odio similique quia illo doloremque distinctio, delectus aperiam veniam unde voluptas? Ab, perferendis facere?"
+      />
+    </div>
+  );
+}
+export default App;
---
+++ src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props) {
+  return (
+    <div>
+      <h1>{props.author}</h1>
+      <p>{props.description}</p>
+    </div>
+  );
+}
