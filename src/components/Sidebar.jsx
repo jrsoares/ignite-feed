cbd68108f8b5d3cc6eb5e7e76761e2ae26ef5734
Jr.Soares <juniorsoaresti@gmail.com>
2022-10-03 16:36:03 -0400

    adjusting profile margin

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ export function Sidebar() {
       </div>
       <footer>
         <a href="#">
-          <PencilLine />
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
