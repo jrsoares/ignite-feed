2b5ae6076e2b592f59b50d7a98745c5968f5c02c
Jr.Soares <juniorsoaresti@gmail.com>
2022-10-17 12:38:26 -0400

    Add key props in Post and Comments Components

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -52,10 +52,10 @@ export function Post({ author, content, publishedAt }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === 'link') {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -79,7 +79,7 @@ export function Post({ author, content, publishedAt }) {
         </footer>
       </form>
       {comments.map((comment) => {
-        return <Comment content={comment} />;
+        return <Comment key={comment} content={comment} />;
       })}
     </article>
   );
